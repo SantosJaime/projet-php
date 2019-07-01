eac327e0d38b86256d2d123c24c608a05ae05ccd
SantosJaime <jaime.santos@hotmail.ch>
2019-07-01 15:43:31 +0200

    Commit1.1

--- initialscript.sql
+++ initialscript.sql
@@ -1,13 +1,16 @@
 CREATE DATABASE AnnuaireToutou;-- Creation Base de Données
 USE AnnuaireToutou;
+
 CREATE USER "adminToutou"@"localhost" IDENTIFIED BY "Annu@ireT0ut0u";--Creation user 
 GRANT ALL PRIVILEGES ON AnnuaireToutou.* TO "adminToutou"@"localhost";--sans droits de creation d'autres utilisateurs
 FLUSH PRIVILEGES;-- Force creation user
+
 CREATE TABLE Maitres (--Creation table
     id INT PRIMARY KEY,
     nom VARCHAR(200),
     telephone VARCHAR(20)
 );
+
 CREATE TABLE Chiens (--Creation table
     id INT PRIMARY KEY,
     nom VARCHAR(255),
---
+++ www/database.php
@@ -0,0 +1,32 @@
+<?php
+Class Database {
+    private $connexion;
+
+    public function __construct(){
+ 
+        $PARAM_hote="mariadb";//chemin vers le serveur
+        $PARAM_port="3306";//port de connexion DB
+        $PARAM_nom_bd="AnnuaireToutou";// Nom DB
+        $PARAM_utilisateur="adminToutou";//nom utilisateur connexion
+        $PARAM_mot_de_passe="Annu@ireT0ut0u";//mot de passe connexion
+
+        try{
+            $this->connexion = new PDO(
+                "mysql:dbname=" .$PARAM_nom_bd,";host=" .$PARAM_hote,
+                $PARAM_utilisateur,,
+                $PARAM_mot_passe );
+        }catch(Exception $monException){
+            echo "Erreur : ".$monException->getMessage() ."<br />";
+            echo "N° : ".$monException->getCode();
+        }
+
+    }
+
+    public function $getConnexion()
+        return $this->connexion;
+    
+}
+
+
+
+?>
